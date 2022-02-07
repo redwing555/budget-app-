7ee5479035692905fea7e28cc3433b2db4eeb4bf
Hichami Houssam <hichami.houssam@gmail.com>
2022-02-07 15:00:40 +0100

    add validations

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,6 @@
 class Group < ApplicationRecord
     has_and_belongs_to_many :operations
     belongs_to :user
+
+    validates :name, :icon, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
 
     has_many :groups, foreign_key: 'user_id'
     has_many :purchases, foreign_key: 'user_id'
+
+    validates_uniqueness_of :name, :email
+    validates :name, :email, presence: true
+    validates :password, length: { minimum: 6 }
 end
