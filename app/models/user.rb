3159322be6fcb070fefccff5bb53467a060a4d7c
Hichami Houssam <hichami.houssam@gmail.com>
2022-02-07 14:59:24 +0100

    add groups-operations joint table + relationships between tables

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+
+    has_many :groups, foreign_key: 'user_id'
+    has_many :purchases, foreign_key: 'user_id'
 end
--- db/migrate/20220207132850_create_operations.rb
+++ db/migrate/20220207132850_create_operations.rb
@@ -3,7 +3,8 @@ class CreateOperations < ActiveRecord::Migration[7.0]
     create_table :operations do |t|
       t.string :name
       t.float :amount
-
+      t.references :user, null: false, foreign_key: true
+      
       t.timestamps
     end
   end
